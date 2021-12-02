c3d36adccd0bb083c592846f7edfd9c7f8bd2770
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2021-12-01 22:42:56 -0300

    WIP: conexao estabelecida

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,14 +1,33 @@
-import sql from 'sequelize'
+import { Sequelize } from 'sequelize'
 
-const sequelize = new sql.Sequelize('postgres://admin:pass@3edsw21qa:5432/comanda-virtual')
+const DB_ADDRESS = '127.0.0.1'
+const DB_PORT = '5432'
+const DB_USER = 'postgres'
+const DB_PASS = '3edsw21qa'
+const DB_NAME = 'comandavirtual'
+const DB_URI =
+	'postgres://' +
+	DB_USER +
+	':' +
+	DB_PASS +
+	'@' +
+	DB_ADDRESS +
+	':' +
+	DB_PORT +
+	'/' +
+	DB_NAME
+
+// const DB_URI = `postgress://${DB_USER}:${DB_PASS}@${DB_ADDRESS}:${DB_PORT}/`
+
+const sequelize = new Sequelize(DB_URI)
 
 async function main() {
-    try {
-        await sequelize.authenticate()
-        console.log('Conexão estabelecida com sucesso');
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		await sequelize.authenticate()
+		console.log('Conexão estabelecida com sucesso')
+	} catch (error) {
+		console.log(error)
+	}
 }
 
 main()
